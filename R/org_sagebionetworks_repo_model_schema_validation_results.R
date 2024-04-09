4ab88ec1188a93a523c066cf245406bc67060a4a
anngvu <anngvu@gmail.com>
2024-04-09 11:37:52 -0600

    Patch R6 issue

--- R/org_sagebionetworks_repo_model_schema_validation_results.R
+++ R/org_sagebionetworks_repo_model_schema_validation_results.R
@@ -321,9 +321,9 @@ OrgSagebionetworksRepoModelSchemaValidationResults <- R6::R6Class(
     #'
     #' @return true if the values in all fields are valid.
     #' @export
-    isValid = function() {
-      TRUE
-    },
+    # isValid = function() {
+    #   TRUE
+    # },
     #' Return a list of invalid fields (if any).
     #'
     #' @description
