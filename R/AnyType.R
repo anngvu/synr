bfbdb79093c337eb326aa0f10762b853d5293f27
Anh Nguyet Vu <anngvu@gmail.com>
2024-07-14 09:51:41 -0700

    Add AnyType stub to fix doc fail, pass check

---
+++ R/AnyType.R
@@ -0,0 +1,9 @@
+#' AnyType object
+#'
+#' API response object `AnyType`.
+#' @title AnyType
+#' @docType class
+#' @name AnyType
+#' @export
+AnyType <- R6::R6Class("AnyType",
+                       public = list())
